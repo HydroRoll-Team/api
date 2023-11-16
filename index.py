d59af728300301a4edbe13d968ba542a2818e25e
简律纯 <i@jyunko.cn>
2023-11-16 22:29:30 +0800

    chore: labalaba

--- index.py
+++ index.py
@@ -1,9 +1,23 @@
-from sanic import Sanic
-from sanic.response import json
-app = Sanic()
- 
- 
-@app.route('/')
-@app.route('/<path:path>')
-async def index(request, path=""):
-    return json({'hello': path})
+import json 
+import random 
+from http.server import BaseHTTPRequestHandler 
+from os.path import dirname, abspath, join
+
+dir = dirname(abspath(__file__))
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+        with open(join(dir, 'data', 'roots.txt'), 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            random_line = random.choice(lines)
+            response = {
+                'line': random_line.strip()
+            }
+            response_json = json.dumps(response, ensure_ascii=False)
+
+            self.wfile.write(response_json.encode('utf-8'))
+        return
