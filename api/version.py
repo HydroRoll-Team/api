68c1a6d0b58cf2a692970952b35f6eae70ea2106
简律纯 <i@jyunko.cn>
2023-11-17 02:44:19 +0800

    fix: 哪出问题了（

--- api/version.py
+++ api/version.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import aiohttp
 
 app = FastAPI()
 
 
 @app.get("/api/version")
-async def hello_world():
-    async with aiohttp.ClientSession() as session:
-        async with session.get("https://pypi.org/pypi/hydroroll/json") as response:
-            data = await response.json()
-            return {"HydroRoll": {"version": data["info"]["version"]}}
+async def get_version():
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://pypi.org/pypi/hydroroll/json") as response:
+                data = await response.json()
+                return {"HydroRoll": {"version": data["info"]["version"]}}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
