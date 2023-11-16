1bc0bc3e77e3d8750fec9164013e748d3e4de412
简律纯 <i@jyunko.cn>
2023-11-16 22:57:44 +0800

    refactor: 基于nextjs+fastapi的重构

--- api/version.py
+++ api/version.py
@@ -1,23 +1,8 @@
-import json 
-import random 
-from http.server import BaseHTTPRequestHandler 
-from os.path import dirname, abspath, join
+from fastapi import FastAPI
 
-dir = dirname(abspath(__file__))
+app = FastAPI()
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
-        with open(join(dir, 'data', 'roots.txt'), 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-            random_line = random.choice(lines)
-            response = {
-                'line': random_line.strip()
-            }
-            response_json = json.dumps(response, ensure_ascii=False)
-
-            self.wfile.write(response_json.encode('utf-8'))
-        return
+@app.get("/api/version")
+@app.get("/api/versions")
+def hello_world():
+    return {"version": "v0.1.4"}
