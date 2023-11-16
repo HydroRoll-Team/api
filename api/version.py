00ffecd860fcb154a338dff602ae54ba994c39bf
简律纯 <i@jyunko.cn>
2023-11-17 02:58:38 +0800

    feat(api/version): 新增`get_target_version()`方法模块化管理

--- api/version.py
+++ api/version.py
@@ -7,9 +7,23 @@ app = FastAPI()
 @app.get("/api/version")
 async def get_version():
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://pypi.org/pypi/hydroroll/json") as response:
-                data = await response.json()
-                return {"HydroRoll": {"version": data["info"]["version"]}}
+        hydroroll = await get_target_version("hydroroll")
+        iamai = await get_target_version("iamai")
+        psi = await get_target_version("psi")
+        hydrorollcore = await get_target_version("hydrorollcore")
+        oneroll = await get_target_version("oneroll")
+        return hydroroll | iamai | psi | oneroll | hydrorollcore
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+
+
+async def get_target_version(pkg_name: str) -> dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://pypi.org/pypi/{pkg_name}/json") as response:
+            try:
+                data = await response.json()
+                latest_version = data["info"]["version"]
+                details = data["releases"][latest_version]
+                return {pkg_name: {"version": latest_version, "details": details}}
+            except Exception as e:
+                return {pkg_name: f"{e}"}
