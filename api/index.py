db6deafe5f77be4f831af8fbad66fba6c04afb4d
Ntkskwk <natsukawa247@outlook.com>
2024-11-19 17:52:05 +0800

    feat(api): add GitHub og:image api

--- api/index.py
+++ api/index.py
@@ -1,7 +1,32 @@
+import asyncio
+import aiohttp
+
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from bs4 import BeautifulSoup
+
 
 app = FastAPI()
+
+# concurrent limit
+semaphore = asyncio.Semaphore(10)
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/github_og")
+async def image():
+    repo_url = "https://github.com/HydroRoll-Team/HydroRoll"
+    conn=aiohttp.TCPConnector(verify_ssl=False)
+    async with semaphore:
+        async with aiohttp.request('GET',repo_url, connector=conn) as resp:
+            if resp.status != 200:
+                return {"message": resp.text(), "status":resp.status }
+            
+            text = await resp.text()
+            soup = BeautifulSoup(text, "html.parser")
+            og_image = soup.find("meta", property="og:image")["content"]
+            return RedirectResponse(url=og_image)
+    
